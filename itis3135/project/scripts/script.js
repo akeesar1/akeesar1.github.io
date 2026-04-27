6772614cabbab4c2e554cf87daffaa1d7cd2167b
akeesar1 <akeesar1@charlotte.edu>
2026-04-26 20:32:03 -0400

    edits

--- itis3135/project/scripts/script.js
+++ itis3135/project/scripts/script.js
@@ -17,28 +17,3 @@ function openProject(url) {
     window.open(url, "_blank");
 }
 
-function validateForm() {
-    let email = document.getElementById("email").value;
-
-    if (!email.includes("@")) {
-        document.getElementById("error").innerText = "Invalid email!";
-        return false;
-    }
-
-    return true;
-}
-
-// AJAX example
-function sendData() {
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-        method: "POST",
-        body: JSON.stringify({
-            name: document.getElementById("name").value
-        }),
-        headers: {
-            "Content-type": "application/json"
-        }
-    })
-    .then(response => response.json())
-    .then(data => console.log(data));
-}
